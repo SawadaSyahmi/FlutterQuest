1fed3d6b8f605453d3ca54fc069526c8c61c45b3
SawadaSyahmi <farissyahmisamidi@gmail.com>
2026-06-07 09:03:43 +0800

    main

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 // Paste your Supabase details here.
 // Supabase Dashboard > Project Settings > API
 window.FLUTTER_QUEST_SUPABASE = {
-  url: "https://fikxsdpijjiavlleibaa.supabase.co/rest/v1/",
+  url: "https://fikxsdpijjiavlleibaa.supabase.co",
   anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZpa3hzZHBpamppYXZsbGVpYmFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3NzcyMzMsImV4cCI6MjA5NjM1MzIzM30.Zc7MyQhYcLI2x-nU0Ay_sc_V2THiePHoxqU96krYFYg"
 };
